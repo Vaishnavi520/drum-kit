827ddf2af8e73be3451526f46e42cd9c55d33eb4
Vaishnavi <125639025+Vaishnavi520@users.noreply.github.com>
2024-10-12 19:28:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,46 +3,65 @@ for(var i=0;i<number;i++){
     document.querySelectorAll(".drum")[i].addEventListener("click",function(){
       // var audio= new Audio("sounds/tom-1.mp3");
       // audio.play();
-    
+      var Buttonclick=this.innerHTML;
+    makesound(Buttonclick);
+    buttonAnimation(Buttonclick);
 
-      var ButtonClick=this.innerHTML;
+    })
 
-      switch (ButtonClick) {
-        case "w":
-          var tom1=new Audio("./sounds/crash.mp3");
+document.addEventListener("keypress",function(event){
+makesound(event.key);
+buttonAnimation(event.key);
+})
+  
+}
+function makesound(key){
+  
+  
+  switch (key) {
+    case "w":
+      var tom1=new Audio("./sounds/crash.mp3");
+      tom1.play();
+      break;
+      case "a":
+        var tom1=new Audio("sounds/kick-bass.mp3");
+        tom1.play();
+        break;
+
+        case "s":
+          var tom1=new Audio("sounds/tom-1.mp3");
           tom1.play();
           break;
-          case "a":
-            var tom1=new Audio("sounds/kick-bass.mp3");
-            tom1.play();
-            break;
-
-            case "s":
-              var tom1=new Audio("sounds/tom-1.mp3");
-              tom1.play();
-              break;
-              case "d":
-          var tom1=new Audio("sounds/tom-2.mp3");
+          case "d":
+      var tom1=new Audio("sounds/tom-2.mp3");
+      tom1.play();
+      break;
+      case "j":
+      var tom1=new Audio("sounds/tom-3.mp3");
+      tom1.play();
+      break;
+      case "k":
+        var tom1=new Audio("sounds/tom-4.mp3");
+        tom1.play();
+        break;
+        case "l":
+          var tom1=new Audio("sounds/snare.mp3");
           tom1.play();
           break;
-          case "j":
-          var tom1=new Audio("sounds/tom-3.mp3");
-          tom1.play();
-          break;
-          case "k":
-            var tom1=new Audio("sounds/tom-4.mp3");
-            tom1.play();
-            break;
-            case "l":
-              var tom1=new Audio("sounds/snare.mp3");
-              tom1.play();
-              break;
-        default:
-          case "j":
-            console.log(ButtonClick);
-          break;
-      }
+    default:
+      case "j":
+        console.log(key);
+      break;
+  }
+}
 
-      
-    });
+
+function buttonAnimation(currentkey){
+ var btn=document.querySelector("."+ currentkey);
+ btn.classList.add("pressed");
+
+ setTimeout(function(){
+  btn.classList.remove("pressed");
+ },100);
+
 }
